d4e944d7cc0baec7c609eaebcba5b50b7cddb267
Masroor Ahmed <masroor.ahmed@devsatmelior.com>
2026-08-06 11:39:34 +0500

    added semi bold font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import {
   LeagueSpartan_300Light,
   LeagueSpartan_400Regular,
   LeagueSpartan_500Medium,
+  LeagueSpartan_600SemiBold,
   LeagueSpartan_700Bold,
   LeagueSpartan_800ExtraBold,
   useFonts
@@ -21,6 +22,7 @@ export default function RootLayout() {
     'LeagueSpartan-Light': LeagueSpartan_300Light,
     'LeagueSpartan-Regular': LeagueSpartan_400Regular,
     'LeagueSpartan-Medium': LeagueSpartan_500Medium,
+    'LeagueSpartan-SemiBold': LeagueSpartan_600SemiBold,
     'LeagueSpartan-Bold': LeagueSpartan_700Bold,
     'LeagueSpartan-ExtraBold': LeagueSpartan_800ExtraBold
   });
